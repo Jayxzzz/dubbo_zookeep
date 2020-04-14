894de99da2782de390e3cda19f4c8c7708aef001
Jayxzzz <1004634213@qq.com>
2020-04-14 18:48:55 +0800

    d盘提交test1

---
+++ zd_provider1/src/main/java/com/java/provider/Test.java
@@ -0,0 +1,13 @@
+package com.java.provider;
+
+/**
+ * author:Jay
+ * time:18:44
+ * date:2020/4/14
+ */
+public class Test {
+
+    public void test1(){
+        System.out.println("test1");
+    }
+}
